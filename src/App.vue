4ca5a1416db3af1ea4288fe1be0c52dace71d0cf
MaretVaabel <61139478+MaretVaabel@users.noreply.github.com>
2020-11-16 13:17:53 +0200

    add

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<template>
+    <div id="app">
+        <Header :cart="cart"/>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    import Header from './components/Header.vue'
+
+    export default {
+
+        name: 'app',
+        methods: {
+
+        },
+        components: {
+            Header,
+        },
+        props: {
+            cart: Object
+        }
+    
+    }
+</script>
+
+<style>
+    * {
+        font-family: 'Open Sans', sans-serif;
+    }
+
+    html, body {
+        margin: 70px 0 0 0;
+        padding: 0;
+        background-color: #f7f7f7;
+    }
+</style>
